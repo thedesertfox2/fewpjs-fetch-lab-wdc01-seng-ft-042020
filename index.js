3195a8d2b6544fc3777a7930ad1221191ac71f0e
thedesertfox2 <alexander.gautherin@uconn.edu>
2020-06-04 22:36:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function fetchBooks() {
-
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json){
+    renderBooks(json)
+  })
+  
 }
 
 function renderBooks(books) {
